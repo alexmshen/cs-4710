e8128a490d9ea5b5a0d0c0e81c3ce2024615fade
alexmshen <98730522+alexmshen@users.noreply.github.com>
2023-11-14 14:04:54 -0500

    added alpaca integration

---
+++ stat_arb.py
@@ -0,0 +1,130 @@
+from config import ALPACA_CONFIG
+from datetime import datetime, timedelta
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.brokers import Alpaca
+from lumibot.strategies import Strategy
+from lumibot.traders import Trader
+import numpy as np
+import pandas as pd
+
+
+class StatArb(Strategy):
+
+    def initialize(self):
+        signal = None
+        start = "2022-01-01"
+
+        self.signal = signal
+        self.start = start
+        self.sleeptime = "1D"
+    # minute bars, make functions    
+
+    def on_trading_iteration(self):
+        stock1 = self.get_historical_prices("AMZN", 22, "day")
+        stock2 = self.get_historical_prices("GOOG", 22, "day")
+        amzn = stock1.df
+        goog = stock2.df
+
+        # Calculate the price ratio between the two stocks
+        ratio = amzn['close'] / goog['close']
+
+        # Calculate the z-score of the price ratio
+        zscore = (ratio - np.mean(ratio)) / np.std(ratio)
+
+        # Generate a signal when the z-score exceeds a certain threshold
+        threshold = 0.5
+
+        '''
+        if zscore > threshold:
+            symbol = "amzn"
+            quantity = 200
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol, quantity, "sell")
+            self.submit_order(order)
+
+            symbol = "goog"
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol, quantity, "buy")
+            self.submit_order(order)
+
+        elif zscore < -threshold:
+            symbol = "amzn"
+            quantity = 200
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol, quantity, "buy")
+            self.submit_order(order)
+
+            symbol = "goog"
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol, quantity, "sell")
+            self.submit_order(order)
+        '''
+
+        if goog['close'] - amzn['close'] >= 5.0:
+            symbol1 = "GOOG"
+            quantity = 200
+            pos = self.get_position(symbol1)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol1, quantity, "sell")
+            self.submit_order(order)
+
+            symbol2 = "AMZN"
+            pos = self.get_position(symbol2)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol2, quantity, "buy")
+            self.submit_order(order)
+
+        elif amzn['close'] - goog['close'] >= 5:
+            symbol1 = "AMZN"
+            quantity = 200
+            pos = self.get_position(symbol1)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol1, quantity, "sell")
+            self.submit_order(order)
+
+            symbol2 = "GOOG"
+            pos = self.get_position(symbol2)
+            if pos is not None:
+                self.sell_all()
+            order = self.create_order(symbol2, quantity, "buy")
+            self.submit_order(order)
+        
+        elif abs(amzn['close'] - goog['close']) <= .1:
+            symbol = "AMZN"
+            quantity = 200
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+
+            symbol = "GOOG"
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+
+if __name__ == "__main__":
+    trade = False
+    if trade:
+        broker = Alpaca(ALPACA_CONFIG)
+        strategy = StatArb(broker=broker)
+        bot = Trader()
+        bot.add_strategy(strategy)
+        bot.run_all()
+    else:
+        start = datetime(2022, 4, 15)
+        end = datetime(2023, 4, 15)
+        StatArb.backtest(
+            YahooDataBacktesting,
+            start,
+            end
+        )
--- test.py
+++ test.py
@@ -1,87 +1,131 @@
-from datetime import datetime
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 import backtrader as bt
 import yfinance as yf
+from random import sample
 
-# Create a subclass of Strategy to define the indicators and logic
-
-class SmaCross(bt.Strategy):
-    # list of parameters which are configurable for the strategy
-    params = dict(
-        pfast=10,  # period for the fast moving average
-        pslow=200   # period for the slow moving average
-    )
-
+# Define the trading strategy
+class DQNStrategy(bt.Strategy):
     def __init__(self):
-        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
-        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
-        self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+        self.data0 = self.datas[0]
+        self.data1 = self.datas[1]
+        self.state_shape = (3,)
+        self.nb_actions = 2
+        self.memory = []
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
 
-    def next(self):
-        if not self.position:  # not in the market
-            if self.crossover > 0:  # if fast crosses slow to the upside
-                self.buy()  # enter long
+    def _build_model(self):
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(16, input_shape=self.state_shape, activation='relu'),
+            tf.keras.layers.Dense(16, activation='relu'),
+            tf.keras.layers.Dense(self.nb_actions, activation='linear')
+        ])
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate),
+              loss='mse', metrics=['mae'])
+        return model
 
-        elif self.crossover < 0:  # in the market & cross to the downside
-            self.close()  # close long position
-
-class Rsi(bt.Strategy):
-    params = dict(
-        period=14,
-        upperband=70,
-        lowerband=30
-    )
-
-    def __init__(self):
-        self.rsi = bt.indicators.RSI(period=self.params.period)
-        self.upperband = self.params.upperband
-        self.lowerband = self.params.lowerband
-
-    def next(self):
-        if not self.position:
-            if self.rsi < self.lowerband:
-                self.buy()
+    def _act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.nb_actions)
         else:
-            if self.rsi > self.upperband:
-                self.sell()
+            return np.argmax(self.model.predict(np.array([state])))
 
-class StatArb(bt.Strategy):
-    params = dict(
-        period=20,
-        threshold=2.0
-    )
+    def _remember(self, state, action, reward, next_state, done):
+        self.memory.append((np.array(state), action, reward, np.array(next_state), done))
+        
+    def _replay(self, batch_size, num_iterations):
+        if len(self.memory) < batch_size:
+            return
 
-    def __init__(self):
-        self.data1 = self.datas[0]
-        self.data2 = self.datas[1]
-        self.spread = self.data1.close - self.data2.close
-        self.sma = bt.indicators.SMA(self.spread, period=self.params.period)
-        self.zscore = (self.spread - self.sma) / bt.indicators.StdDev(self.spread, period=self.params.period)
+        minibatch = sample(self.memory, batch_size)
+        states = np.array([x[0] for x in minibatch])
+        actions = np.array([x[1] for x in minibatch])
+        rewards = np.array([x[2] for x in minibatch])
+        next_states = np.array([x[3] for x in minibatch])
+        dones = np.array([x[4] for x in minibatch])
+
+        for i in range(num_iterations):
+            # Update the target model
+            targets = rewards + self.gamma * np.amax(self.model.predict(next_states), axis=1) * (1 - dones)
+            target_f = self.model.predict(states)
+            target_f[np.arange(batch_size), actions] = targets
+            self.model.fit(states, target_f, epochs=1, verbose=0)
+
+        # Update the epsilon value
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
     def next(self):
-        if self.zscore[0] > self.params.threshold:
-            self.sell(data=self.data1)
-            self.buy(data=self.data2)
-        elif self.zscore[0] < -self.params.threshold:
-            self.sell(data=self.data2)
-            self.buy(data=self.data1)
+        # Calculate the moving average
+        ma_period = 20
+        close0 = self.data0.close.get(size=ma_period)
+        close1 = self.data1.close.get(size=ma_period)
 
+        ma0 = np.mean(close0)
+        ma1 = np.mean(close1)
+
+        # Calculate the z-score
+        zscore0 = (self.data0.close[0] - ma0) / np.std(self.data0.close.get(size=ma_period), ddof=1)
+        zscore1 = (self.data1.close[0] - ma1) / np.std(self.data1.close.get(size=ma_period), ddof=1)
+
+        # Update the state
+        state = np.array([zscore0, zscore1, self.data0.close[0] / self.data1.close[0]])
+
+        # Get the action from the agent
+        action = self._act(state)
+
+        # Execute the action based on the DQN strategy
+        if action == 0:
+            self.buy(data=self.data0, size=20)
+            self.sell(data=self.data1, size=20)
+        else:
+            self.sell(data=self.data0, size=20)
+            self.buy(data=self.data1, size=20)
+
+        # Check if the two stocks converge
+        if abs(self.data0.close[0] - self.data1.close[0]) < 0.01 * (self.data0.close[0] + self.data1.close[0]) / 2:
+            # Reverse the trade
+            if action == 0:
+                self.sell(data=self.data0, size=20)
+                self.buy(data=self.data1, size=20)
+            else:
+                self.buy(data=self.data0, size=20)
+                self.sell(data=self.data1, size=20)
+
+        # Calculate the reward
+        reward = self.broker.getvalue()
+
+        # Update the memory
+        next_state = np.array([zscore0, zscore1, self.data0.close[0] / self.data1.close[0]])
+        done = False
+        self._remember(state, action, reward, next_state, done)
+
+        # Update the agent
+        self._replay(batch_size=32, num_iterations=10)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            
+# Create the cerebro engine
 cerebro = bt.Cerebro()
 
-data1 = bt.feeds.PandasData(dataname=yf.download('COKE', '2020-01-01', '2023-11-09'))
-data2 = bt.feeds.PandasData(dataname=yf.download('PEP', '2020-01-01', '2023-11-09'))
+# Add the data feeds
+data1 = bt.feeds.PandasData(dataname=yf.download('COKE', '2023-08-08', '2023-11-09'))
+data2 = bt.feeds.PandasData(dataname=yf.download('PEP', '2023-08-08', '2023-11-09'))
 
 cerebro.adddata(data1)
 cerebro.adddata(data2)
 
-cerebro.addstrategy(StatArb)
 
-# Create a data feed
-'''
-data = bt.feeds.PandasData(dataname=yf.download('TSLA', '2020-01-01', '2023-11-09'))
-cerebro.adddata(data)  # Add the data feed
 
-cerebro.addstrategy(SmaCross)  # Add the trading strategy
-cerebro.addstrategy(Rsi)
-'''
-cerebro.run()  # run it all
-cerebro.plot()  # and plot it with a single command
+# Add the strategy
+cerebro.addstrategy(DQNStrategy)
+
+# Run the backtest
+cerebro.run()
+cerebro.plot()
